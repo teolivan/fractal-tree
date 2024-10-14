8d3f1fd665908ad5002a3e0b6e7bbf83d62c17a4
Оливия <74550333+teolivan@users.noreply.github.com>
2024-10-14 22:05:23 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,34 @@
+
+
+let angle;
+let slider;
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(900, 800);
+  slider = createSlider(0, TWO_PI, PI / 4, 0.01);
 }
 
 function draw() {
-  background(220);
+  background(50);
+  angle = slider.value();
+  stroke(100,220,70);
+  strokeWeight(0.5);
+  translate(width * 0.5, height);
+  branch(250);
+}
+
+function branch(len) {
+  line(0, 0, 0, -len);
+  translate(0, -len);
+  
+  if (len > PI) {
+    push();
+    rotate(angle);
+    branch(len * (2/3));
+    pop();
+    push();
+    rotate(-angle);
+    branch(len * (2/3));
+    pop();
+  }
 }
